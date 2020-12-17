9ac69c5e06445f8961c30536a1fd6a48b928db71
JorganBonstragel <65880029+JorganBonstragel@users.noreply.github.com>
2020-12-17 10:11:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,54 +2,54 @@
 const projectArray = [
     {
         title:"Simple Calculator",
-        img:"img/Calculator.PNG",
+        img:"img/Calculator.png",
         live:'pages/Simple-Calculator-main/index.html',
-        github:'https://github.com/JorganBonstragel/Simple-Calculator',
+        github:'https://github.com/ChrsSampson/Simple-Calculator',
         text:"A vanilla javascript calculator featuring a light and dark mode for extra flair",
         icon: "icons/calculator.svg",
         tags:['css', 'javascript', 'html'],
     },
     {
         title:"Web Dashboard",
-        img:"img/Dashboard.PNG",
+        img:"img/Dashboard.png",
         live:'pages/Dashboard-main/index.html',
-        github:'https://github.com/JorganBonstragel/Dashboard',
+        github:'https://github.com/ChrsSampson/Dashboard',
         text:"A web dashboard using Chart.js and simulated data to create charts, graphs, and alerts",
         icon: "icons/chart.svg",
         tags:['mobile-first', 'css', 'javascript', 'html'],
     },
     {
         title:"Game Show App",
-        img:"img/Gameshow.PNG",
+        img:"img/Gameshow.png",
         live:'pages/Game-Show-App-main/index.html',
-        github:'https://github.com/JorganBonstragel/Game-Show-App',
+        github:'https://github.com/ChrsSampson/Game-Show-App',
         text:'A "Hangman" style game where you guess the secret phrase, using vanilla javascript.',
         icon: "icons/puzzle.svg",
         tags:['javascript', 'css', 'html'],
     },
     {
         title:"Employee Directory",
-        img:"img/EmployeeDir.PNG",
+        img:"img/EmployeeDir.png",
         live:'pages/Employee-Directory-main/index.html',
-        github:'https://github.com/JorganBonstragel/Employee-Directory',
+        github:'https://github.com/ChrsSampson/Employee-Directory',
         text:"A twelve person directory using fetch to pull user data from a random user API, also features a modal to flip through users.",
         icon: "icons/users.svg",
         tags:['javascript', 'css', 'html'],
     },
     {
         title:"Chat App",
-        img:"img/ChatApp.PNG",
+        img:"img/ChatApp.png",
         live:'pages/Chat-App-main/index.html',
-        github:'https://github.com/JorganBonstragel/Chat-App',
+        github:'https://github.com/ChrsSampson/Chat-App',
         text:"A chat app mockup that just writes user input to the chat history",
         icon: "icons/comments.svg",
         tags:['css', 'javascript', 'html'],
     },
     {
         title:"Photo Gallery",
-        img:"img/Gallery.PNG",
+        img:"img/Gallery.png",
         live:'pages/Photo-Gallery-main/index.html',
-        github:'https://github.com/JorganBonstragel/Photo-Gallery',
+        github:'https://github.com/ChrsSampson/Photo-Gallery',
         text:"A small photo gallery with using BaguetteBox.js and a custom javascript search function.",
         icon: "icons/photo.svg",
         tags:['javascript', 'css', 'html'],
@@ -73,8 +73,8 @@ projectArray.forEach(item => {
         <p>${item.text}</p>
         <div class="tag-container">${tags}</div>
         <div class="card-link-container">
-            <div class="demo-link"><img class="live-icon" src="icons/demo.svg"><a href=${item.live} class="card-link">Live Demo</a></div>
-            <div class="source-link"><img class="live-icon" src="icons/github.svg"><a href=${item.github} class="card-link">Source Code</a></div>
+            <div class="demo-link"><img class="live-icon" src="/icons/demo.svg"><a href=${item.live} class="card-link">Live Demo</a></div>
+            <div class="source-link"><img class="live-icon" src="/icons/github.svg"><a href=${item.github} class="card-link">Source Code</a></div>
         </div>
     </article>
     `
